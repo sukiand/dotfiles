68c16c3c4aaff1dcdb2ec93d799217618bb5b8ec
Changyun Gong <cgong4@jhu.edu>
2015-03-04 15:09:39 -0800

    aha~

--- .my_shell_scripts/custom.sh
+++
@@ -1,8 +0,0 @@
-
-export GREP_OPTIONS="--col=auto"
-export ANDROID_HOME=/usr/local/opt/android-sdk
-
-export PATH=$PATH:/usr/local/opt/go/libexec/bin
-export GOPATH='/Users/zzy/Documents/Projects/go'
-export PATH=${PATH}:${GOPATH}/bin
-
--- .my_shell_scripts/functions.sh
+++
@@ -1,9 +0,0 @@
-update_pip()
-{
-  pip freeze --local | cut -d = -f 1 | xargs pip install -U
-}
-
-change_java_encoding_to_utf8()
-{
-  export JAVA_TOOL_OPTIONS='-Dfile.encoding=UTF-8'
-}
