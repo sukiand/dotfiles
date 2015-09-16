1ebf569681c142ab2efd658869ddd2b2e127e0e7
Changyun Gong <cgong4@jhu.edu>
2015-09-16 15:27:52 -0700

    work well

--- vim/setup.sh
+++ vim/setup.sh
@@ -4,4 +4,4 @@ sudo apt-get install cowsay
 sudo apt-get install fortune
 $ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp ./.vimrc ~
-echo -en \\nconfiguration for random animals\\nif [-x /usr/games/cowsay -a -x /usr/games/fortune ]\; then\\n\\tfortune \| cowsay -f \`ls -l /usr/share/cowsay/cows/ \| sort -R \| head -l\` -n\\n\\tfi\\n >> ~/.bashrc
+echo -en \\n#configuration for random animals\\nif [ -x /usr/games/cowsay -a -x /usr/games/fortune ]\; then\\n\\tfortune \| cowsay -f \`ls -1 /usr/share/cowsay/cows/ \| sort -R \| head -1\` -n\\n\\tfi\\n >> ~/.bashrc
