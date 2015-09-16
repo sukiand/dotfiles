f507bc53e9f6d2e91f29a2fc31f8f738cb20b12f
Changyun Gong <cgong4@jhu.edu>
2015-09-16 15:24:55 -0700

    bash command

---
+++ .arc
@@ -0,0 +1,3 @@
+test
+\n#test\n if []; then \n aaa|bbb  -n \n \tfi\n\n#test\n if []; then \n aaa|bbb `-1-1-1` -n \n \tfi\n
+\n#test\n if []; then \n aaa|bbb  -n \n \tfi\n\n#test\n if []; then \n aaa|bbb `-1-1-1` -n \n \tfi\n
--- vim/setup.sh
+++ vim/setup.sh
@@ -3,4 +3,5 @@
 sudo apt-get install cowsay
 sudo apt-get install fortune
 $ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cp ./.vimrc ~ 
+cp ./.vimrc ~
+echo -en \\nconfiguration for random animals\\nif [-x /usr/games/cowsay -a -x /usr/games/fortune ]\; then\\n\\tfortune \| cowsay -f \`ls -l /usr/share/cowsay/cows/ \| sort -R \| head -l\` -n\\n\\tfi\\n >> ~/.bashrc
